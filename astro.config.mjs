407bc337069d3358404e2f2adfbaabd566454d43
oding-ai <tkksm3@gmail.com>
2026-02-06 19:55:12 +0900

    feat: TOC 생성을 위한 rehype 플러그인(slug, autolink) 설치 및 설정

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,19 +3,24 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+
+import rehypeSlug from 'rehype-slug';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [
     mdx({
-      // Shiki 설정 (Syntax Highlighting)
       shikiConfig: {
-        theme: 'dracula', // 다크 모드에 어울리는 테마
-        wrap: true,       // 긴 줄 자동 줄바꿈
+        theme: 'dracula', 
+        wrap: true,
       },
+      rehypePlugins: [
+        rehypeSlug, // 헤더에 id 자동 생성 (#header)
+        [rehypeAutolinkHeadings, { behavior: 'wrap' }], // 헤더에 링크 걸기
+      ],
     }), 
     sitemap()
   ],
