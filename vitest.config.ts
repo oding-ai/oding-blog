f73c5e893aa2957f3c02e5bc26b6a282095a1aea
oding-ai <tkksm3@gmail.com>
2026-02-07 21:44:52 +0900

    feat: add vitest and header component test

---
+++ vitest.config.ts
@@ -0,0 +1,10 @@
+/// <reference types="vitest" />
+import { getViteConfig } from 'astro/config';
+
+export default getViteConfig({
+  test: {
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.ts',
+    include: ['src/**/*.{test,spec}.{js,ts,jsx,tsx}'],
+  },
+});
